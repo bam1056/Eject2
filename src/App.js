e0dd9a7039e97398f6f6baaeeb1efbeb9cc422ef
Brett Macy <bam03584@aol.com>
2016-08-23 14:07:20 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import './Style.sass'
+
+class App extends Component {
+  render () {
+    return <div className="App">
+      <h1>This is my App</h1>
+      <p>This is my app again</p>
+      <p>This is my app updating on Webpack free of create-react-dependencies</p>
+    </div>
+  }
+}
+export default App
